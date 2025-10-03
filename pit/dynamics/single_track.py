9278b69b14482c33cd7e47b4790738885a2e14bd
Nandan Tumu <nandan@nandantumu.com>
2025-10-03 01:24:19 -0400

    Modify dynamics to use steering vel

--- pit/dynamics/single_track.py
+++ pit/dynamics/single_track.py
@@ -31,6 +31,84 @@ class SingleTrack(Dynamics, nn.Module):
         """Get the evaluated ODEs of the state at this point
 
         Args:
+            states (): Shape of (B, 7) or (7)
+                [X, Y, STEER, V, YAW, YAW_RATE, SLIP_ANGLE]
+            control_inputs (): Shape of (B, 2) or (2)
+                [STEER_V, ACCEL]
+        """
+        batch_mode = True if len(states.shape) == 2 else False
+        X, Y, STEER, V, YAW, YAW_RATE, SLIP_ANGLE = 0, 1, 2, 3, 4, 5, 6
+        STEER_V, ACCEL = 0, 1
+        diff = torch.zeros_like(states)
+        diff[..., X] = states[..., V] * torch.cos(
+            states[..., YAW] + states[..., SLIP_ANGLE]
+        )
+        diff[..., Y] = states[..., V] * torch.sin(
+            states[..., YAW] + states[..., SLIP_ANGLE]
+        )
+        diff[..., STEER] = control_inputs[..., STEER_V]
+        diff[..., YAW] = states[..., YAW_RATE]
+        diff[..., V] = control_inputs[..., ACCEL]
+        glr = self.g * params["lr"] - control_inputs[..., ACCEL] * params["hcg"]
+        glf = self.g * params["lf"] + control_inputs[..., ACCEL] * params["hcg"]
+        diff[..., YAW_RATE] = (
+            (params["mu"] * params["m"])
+            / (params["Iz"] * (params["lf"] + params["lr"]))
+        ) * (
+            params["lf"] * params["Csf"] * glr * states[..., STEER]
+            + (params["lr"] * params["Csr"] * glf - params["lf"] * params["Csf"] * glr)
+            * states[..., SLIP_ANGLE]
+            - (
+                params["lf"] * params["lf"] * params["Csf"] * glr
+                + params["lr"] * params["lr"] * params["Csr"] * glf
+            )
+            * (
+                states[..., YAW_RATE]
+                / (self.numeric_stability_constant + states[..., V])
+            )
+        )
+
+        diff[..., SLIP_ANGLE] = (
+            params["mu"] / (states[..., V] * (params["lr"] + params["lf"]))
+        ) * (
+            params["Csf"] * glr * states[..., STEER]
+            - (params["Csr"] * glf + params["Csf"] * glr) * states[..., SLIP_ANGLE]
+            + (params["Csr"] * glf * params["lr"] - params["Csf"] * glr * params["lf"])
+            * (
+                states[..., YAW_RATE]
+                / (self.numeric_stability_constant + states[..., V])
+            )
+        ) - states[..., YAW_RATE]
+
+        return diff
+
+
+class SingleTrackMod(Dynamics, nn.Module):
+    """
+    This is the Single Track model, from the CommonRoad paper.
+    Link: https://gitlab.lrz.de/tum-cps/commonroad-vehicle-models/-/blob/master/vehicleModels_commonRoad.pdf
+    """
+
+    def __init__(self, m, Iz, lf, lr, hcg, Csf, Csr, mu, **kwargs) -> None:
+        super().__init__()
+        self.parameter_list = ["m", "Iz", "lf", "lr", "hcg", "Csf", "Csr", "mu"]
+        self.initial_values = {
+            "m": m,
+            "Iz": Iz,
+            "lf": lf,
+            "lr": lr,
+            "hcg": hcg,
+            "Csf": Csf,
+            "Csr": Csr,
+            "mu": mu,
+        }
+        self.g = 9.81
+        self.numeric_stability_constant = 1e-10
+
+    def forward(self, states, control_inputs, params: ParameterSample):
+        """Get the evaluated ODEs of the state at this point
+
+        Args:
             states (): Shape of (B, 6) or (6)
                 [X, Y, V, YAW, YAW_RATE, SLIP_ANGLE]
             control_inputs (): Shape of (B, 2) or (2)
